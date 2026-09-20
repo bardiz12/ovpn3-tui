abbb25f385a61fe31090b65677fee126bf17229f
Bardizba <moch.bardizba@gmail.com>
2026-09-20 23:39:33 +0700

    feat: add Nix flake support

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "A modern Terminal User Interface (TUI) for OpenVPN 3 Linux";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "ovpn3-tui";
+            version = "0.1.0";
+
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+
+            meta = with pkgs.lib; {
+              description = "Terminal User Interface for OpenVPN 3 Linux";
+              homepage = "https://github.com/bardiz12/ovpn3-tui";
+              license = licenses.mit;
+              mainProgram = "ovpn3-tui";
+            };
+          };
+          ovpn3-tui = self.packages.${system}.default;
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/ovpn3-tui";
+        };
+      });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              rustc
+              clippy
+              rustfmt
+              pkg-config
+            ];
+          };
+        }
+      );
+    };
+}
